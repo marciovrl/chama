c6af7227c06d5f09f7811e61a0fe83ba26af0b34
marcio lemos <contato.marciovrl@gmail.com>
2020-11-23 09:27:17 -0300

    add login testcase

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,17 +1,26 @@
 /// <reference types="cypress" />
 
+// ***********************************************************
+// This command is for login in the website.
+// ***********************************************************
 Cypress.Commands.add('login', (credentials) => {
   cy.get('#email').type(credentials.name);
   cy.get('#passwd').type(credentials.password);
   cy.get('#SubmitLogin').click();
 });
 
+// ***********************************************************
+// This command is for add product in to cart and do checkout.
+// ***********************************************************
 Cypress.Commands.add('addProdutcToCart', () => {
   cy.get("a[data-id-product= '1']").click({ multiple: true, force: true });
   cy.get("[title='Proceed to checkout']").click();
   cy.get('.cart_navigation > .button > span').click();
 });
 
+// ***********************************************************
+// This command is for complete all the steps of the checkout.
+// ***********************************************************
 Cypress.Commands.add('doOrder', () => {
   cy.get("[name='processAddress']").click();
   cy.get('#cgv').check();
